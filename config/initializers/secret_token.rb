8a1ba022c7e0bc0e2d54eaf60256816456a8ea3c
Sivasubramanian <isolatednet@gmail.com>
2011-02-13 02:20:42 -0800

    Baseline version of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '760605fc9e2128741b640bb2e0d4833970cb2b7c4e0f7f447e4946f8f36a6d5aaf1650c1986c680316957bf33826db85072e2762dad91ea3d66b0f94bc75a9a2'
---
+++ generate/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_generate_session',
+  :secret      => '227085cceeee80ce188e7552069f17d41060bf259acf71bf45538ea64e4e4bf36fa53e1e91701519a1927b4193782baf9feee042b8ee59b04ae6cf1221857bd0'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
